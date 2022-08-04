839cd7b382754e3d477de82e3b6a884bcfcd1b08
Darby Smith <newman.darby2@gmail.com>
2022-08-04 13:26:53 -0400

    Add answers

--- hashes_2.rb
+++ hashes_2.rb
@@ -7,6 +7,9 @@ states = {
   "Colorado" => "CO"
 }
 
+states.each do |state, abbreviation|
+  p "#{state} has an abbreviation of #{abbreviation}."
+end
 # Use the each method to iterate through it and print out a sentence stating each state name and abbreviation
 #
 #
@@ -23,6 +26,10 @@ birthdays = {
   "Jeff" => "February 12"
 }
 
+birthdays.each do |name, date|
+  p "Did you know #{name}'s birthday is on #{date}?"
+end
+
 # Use the each method to iterate through it and print out a sentence stating each friends name and birthday
 #
 #
@@ -38,7 +45,11 @@ login_statuses = {
   "Amos" => true,
   "Jeff" => true
 }
-
+login_statuses.each do |name, login_status|
+  if login_status == true
+    p name
+  end
+end
 # Use the each method to iterate through it and print ONLY the names of friends who are "logged in"
 #
 #
